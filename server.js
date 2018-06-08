fa0a3074b7482297e7403b1ec34770c38976ca70
Nikolaj K <karpenko.n.a@gmail.com>
2018-06-08 17:17:56 +0300

    work08-06-2

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 //var https = require('http-https');
 var express = require('express');
 var session = require('express-session');
+var bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
 var mongoose = require('mongoose');
 const MongoStore = require('connect-mongo')(session);
 var multer  = require('multer');
+
 var storage = multer.diskStorage({
     destination: function(req, file, callback) {
         callback(null, './upload/img/')
@@ -21,5 +23,14 @@ var storage = multer.diskStorage({
 });
 var app = express();
 var config = require('./config/config');
+var index=require('./rout/index');
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use(express.static('./upload'));
+app.use(express.static('static'));
+app.use(bodyParser.urlencoded({ extended: false, uploadDir: './upload' }));
+app.use(cookieParser());
+
+app.use('/', index);
 
 app.listen(config.port);
