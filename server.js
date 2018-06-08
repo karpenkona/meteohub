fdb5662a25bd6b0b80fc2667fbe29e9959acd2c5
Nikolaj K <karpenko.n.a@gmail.com>
2018-06-08 16:20:35 +0300

    com6

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
-/**
- * Created by karpenko on 08.06.2018.
- */
+//var https = require('http');
+//var https = require('http-https');
+var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
+var multer  = require('multer');
+var storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './upload/img/')
+    },
+    filename: function(req, file, callback) {
+        var a = file.originalname.split('.')[0];
+        callback(null, a + '-' + Date.now() + path.extname(file.originalname))
+    },
+    limits: {
+        fieldNameSize: 100,
+        files: 4,
+        fields: 10}
+});
+var app = express();
+var config = require('./config/config');
+
+app.listen(config.port);
